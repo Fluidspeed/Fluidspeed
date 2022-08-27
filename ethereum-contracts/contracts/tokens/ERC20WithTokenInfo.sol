35358655ec13c1afc63a8cca41fa99cf3d0c11f1
Fluidspeed <104890961+Fluidspeed@users.noreply.github.com>
2022-08-27 16:41:58 +0800

    Delete ERC20WithTokenInfo.sol

--- ethereum-contracts/contracts/tokens/ERC20WithTokenInfo.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPLv3
-pragma solidity >= 0.8.0;
-
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { TokenInfo } from "./TokenInfo.sol";
-
-
-abstract contract ERC20WithTokenInfo is IERC20, TokenInfo {}
