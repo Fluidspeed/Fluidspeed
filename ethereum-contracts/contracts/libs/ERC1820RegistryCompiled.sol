d403589bfee05c5475e6d8a0a63a5142795a2220
Fluidspeed <104890961+Fluidspeed@users.noreply.github.com>
2022-08-27 16:32:29 +0800

    create lib

---
+++ ethereum-contracts/contracts/libs/ERC1820RegistryCompiled.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPLv3
+// solhint-disable const-name-snakecase
+// solhint-disable max-line-length
+pragma solidity 0.8.14;
+
+/// @dev This is meant to be used by test framework to get the raw bytecode without compiling the origin contract
+library ERC1820RegistryCompiled {
+    address internal constant at = address(0x1920a4B7698BdE71Dce8cdc75aAA6C95905faD24);
+    bytes internal constant bin = bytes(hex"608060405234801561001057600080fd5b506004361065b600080fd5b6100e060048036036060812526004820183905260009182919060208160248189617530fa90519096909550935050505056fea165627a7a72305820377f4a2d4301ede9949f163f319021a6e9c687c292a5e2b2c4734c126b524e6c0029");
+}
