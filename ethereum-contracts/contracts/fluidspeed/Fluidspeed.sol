2c718ee51273cba4d81e9d7ce380b71c72c647f5
Fluidspeed <104890961+Fluidspeed@users.noreply.github.com>
2022-09-02 17:37:46 +0800

    0.8.0

--- ethereum-contracts/contracts/fluidspeed/Fluidspeed.sol
+++ ethereum-contracts/contracts/fluidspeed/Fluidspeed.sol
@@ -220,4 +220,513 @@ contract Fluidspeed is
         }
         _;
     }
+    
+    function updateAgreementClass(ISuperAgreement agreementClassLogic) external onlyGovernance override {
+        if (NON_UPGRADABLE_DEPLOYMENT) revert HOST_NON_UPGRADEABLE();
+        bytes32 agreementType = agreementClassLogic.agreementType();
+        uint idx = _agreementClassIndices[agreementType];
+        if (idx == 0) {
+            revert FluidspeedErrors.DOES_NOT_EXIST(FluidspeedErrors.HOST_AGREEMENT_IS_NOT_REGISTERED);
+        }
+        UUPSProxiable proxiable = UUPSProxiable(address(_agreementClasses[idx - 1]));
+        proxiable.updateCode(address(agreementClassLogic));
+        emit AgreementClassUpdated(agreementType, address(agreementClassLogic));
+    }
+
+    function isAgreementTypeListed(bytes32 agreementType)
+        external view override
+        returns (bool yes)
+    {
+        uint idx = _agreementClassIndices[agreementType];
+        return idx != 0;
+    }
+
+    function isAgreementClassListed(ISuperAgreement agreementClass)
+        public view override
+        returns (bool yes)
+    {
+        bytes32 agreementType = agreementClass.agreementType();
+        uint idx = _agreementClassIndices[agreementType];
+        // it should also be the same agreement class proxy address
+        return idx != 0 && _agreementClasses[idx - 1] == agreementClass;
+    }
+
+    function getAgreementClass(bytes32 agreementType)
+        external view override
+        returns(ISuperAgreement agreementClass)
+    {
+        uint idx = _agreementClassIndices[agreementType];
+        if (idx == 0) {
+            revert FluidspeedErrors.DOES_NOT_EXIST(FluidspeedErrors.HOST_AGREEMENT_IS_NOT_REGISTERED);
+        }
+        return ISuperAgreement(_agreementClasses[idx - 1]);
+    }
+
+    function mapAgreementClasses(uint256 bitmap)
+        external view override
+        returns (ISuperAgreement[] memory agreementClasses) {
+        uint i;
+        uint n;
+        // create memory output using the counted size
+        agreementClasses = new ISuperAgreement[](_agreementClasses.length);
+        // add to the output
+        n = 0;
+        for (i = 0; i < _agreementClasses.length; ++i) {
+            if ((bitmap & (1 << i)) > 0) {
+                agreementClasses[n++] = _agreementClasses[i];
+            }
+        }
+        // resize memory arrays
+        assembly { mstore(agreementClasses, n) }
+    }
+
+    function addToAgreementClassesBitmap(uint256 bitmap, bytes32 agreementType)
+        external view override
+        returns (uint256 newBitmap)
+    {
+        uint idx = _agreementClassIndices[agreementType];
+        if (idx == 0) {
+            revert FluidspeedErrors.DOES_NOT_EXIST(FluidspeedErrors.HOST_AGREEMENT_IS_NOT_REGISTERED);
+        }
+        return bitmap | (1 << (idx - 1));
+    }
+
+    function removeFromAgreementClassesBitmap(uint256 bitmap, bytes32 agreementType)
+        external view override
+        returns (uint256 newBitmap)
+    {
+        uint idx = _agreementClassIndices[agreementType];
+        if (idx == 0) {
+            revert FluidspeedErrors.DOES_NOT_EXIST(FluidspeedErrors.HOST_AGREEMENT_IS_NOT_REGISTERED);
+        }
+        return bitmap & ~(1 << (idx - 1));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Super Token Factory
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function getSuperTokenFactory()
+        external view override
+        returns (ISuperTokenFactory factory)
+    {
+        return _superTokenFactory;
+    }
+
+    function getSuperTokenFactoryLogic()
+        external view override
+        returns (address logic)
+    {
+        assert(address(_superTokenFactory) != address(0));
+        if (NON_UPGRADABLE_DEPLOYMENT) return address(_superTokenFactory);
+        else return UUPSProxiable(address(_superTokenFactory)).getCodeAddress();
+    }
+
+    function updateSuperTokenFactory(ISuperTokenFactory newFactory)
+        external override
+        onlyGovernance
+    {
+        if (address(_superTokenFactory) == address(0)) {
+            if (!NON_UPGRADABLE_DEPLOYMENT) {
+                // initialize the proxy
+                UUPSProxy proxy = new UUPSProxy();
+                proxy.initializeProxy(address(newFactory));
+                _superTokenFactory = ISuperTokenFactory(address(proxy));
+            } else {
+                _superTokenFactory = newFactory;
+            }
+            _superTokenFactory.initialize();
+        } else {
+            if (NON_UPGRADABLE_DEPLOYMENT) revert HOST_NON_UPGRADEABLE();
+            UUPSProxiable(address(_superTokenFactory)).updateCode(address(newFactory));
+        }
+        emit SuperTokenFactoryUpdated(_superTokenFactory);
+    }
+
+    function updateSuperTokenLogic(ISuperToken token)
+        external override
+        onlyGovernance
+    {
+        address code = address(_superTokenFactory.getSuperTokenLogic());
+        // assuming it's uups proxiable
+        UUPSProxiable(address(token)).updateCode(code);
+        emit SuperTokenLogicUpdated(token, code);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // App Registry
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function registerApp(
+        uint256 configWord
+    )
+        external override
+    {
+        // check if whitelisting required
+        if (APP_WHITE_LISTING_ENABLED) {
+            revert HOST_NO_APP_REGISTRATION_PERMISSIONS();
+        }
+        _registerApp(configWord, ISuperApp(msg.sender), true);
+    }
+
+    function registerAppWithKey(uint256 configWord, string calldata registrationKey)
+        external override
+    {
+        if (APP_WHITE_LISTING_ENABLED) {
+            bytes32 configKey = FluidspeedGovernanceConfigs.getAppRegistrationConfigKey(
+                // solhint-disable-next-line avoid-tx-origin
+                tx.origin,
+                registrationKey
+            );
+            // check if the key is valid and not expired
+            if (
+                _gov.getConfigAsUint256(
+                    this,
+                    IFluidspeedToken(address(0)),
+                    configKey
+                // solhint-disable-next-line not-rely-on-time
+                ) < block.timestamp) revert HOST_INVALID_OR_EXPIRED_SUPER_APP_REGISTRATION_KEY();
+        }
+        _registerApp(configWord, ISuperApp(msg.sender), true);
+    }
+
+    function registerAppByFactory(
+        ISuperApp app,
+        uint256 configWord
+    )
+        external override
+    {
+        // msg sender must be a contract
+        {
+            uint256 cs;
+            // solhint-disable-next-line no-inline-assembly
+            assembly { cs := extcodesize(caller()) }
+            if (cs == 0) revert FluidspeedErrors.MUST_BE_CONTRACT(FluidspeedErrors.HOST_MUST_BE_CONTRACT);
+        }
+
+        if (APP_WHITE_LISTING_ENABLED) {
+            // check if msg sender is authorized to register
+            bytes32 configKey = FluidspeedGovernanceConfigs.getAppFactoryConfigKey(msg.sender);
+            bool isAuthorizedAppFactory = _gov.getConfigAsUint256(
+                this,
+                IFluidspeedToken(address(0)),
+                configKey) == 1;
+
+            if (!isAuthorizedAppFactory) revert HOST_UNAUTHORIZED_SUPER_APP_FACTORY();
+        }
+        _registerApp(configWord, app, false);
+    }
+
+    function _registerApp(uint256 configWord, ISuperApp app, bool checkIfInAppConstructor) private
+    {
+        // solhint-disable-next-line avoid-tx-origin
+        if (msg.sender == tx.origin) {
+            revert FluidspeedErrors.APP_RULE(SuperAppDefinitions.APP_RULE_NO_REGISTRATION_FOR_EOA);
+        }
+
+        if (checkIfInAppConstructor) {
+            uint256 cs;
+            // solhint-disable-next-line no-inline-assembly
+            assembly { cs := extcodesize(app) }
+            if (cs != 0) {
+                revert FluidspeedErrors.APP_RULE(SuperAppDefinitions.APP_RULE_REGISTRATION_ONLY_IN_CONSTRUCTOR);
+            }
+        }
+        if (
+            !SuperAppDefinitions.isConfigWordClean(configWord) ||
+            SuperAppDefinitions.getAppCallbackLevel(configWord) == 0 ||
+            (configWord & SuperAppDefinitions.APP_JAIL_BIT) != 0
+            ) {
+                revert HOST_INVALID_CONFIG_WORD();
+            }
+        if (_appManifests[ISuperApp(app)].configWord != 0) revert HOST_SUPER_APP_ALREADY_REGISTERED();
+        _appManifests[ISuperApp(app)] = AppManifest(configWord);
+        emit AppRegistered(app);
+    }
+
+    function isApp(ISuperApp app) public view override returns(bool) {
+        return _appManifests[app].configWord > 0;
+    }
+
+    function getAppCallbackLevel(ISuperApp appAddr) public override view returns(uint8) {
+        return SuperAppDefinitions.getAppCallbackLevel(_appManifests[appAddr].configWord);
+    }
+
+    function getAppManifest(
+        ISuperApp app
+    )
+        external view override
+        returns (
+            bool isSuperApp,
+            bool isJailed,
+            uint256 noopMask
+        )
+    {
+        AppManifest memory manifest = _appManifests[app];
+        isSuperApp = (manifest.configWord > 0);
+        if (isSuperApp) {
+            isJailed = SuperAppDefinitions.isAppJailed(manifest.configWord);
+            noopMask = manifest.configWord & SuperAppDefinitions.AGREEMENT_CALLBACK_NOOP_BITMASKS;
+        }
+    }
+
+    function isAppJailed(
+        ISuperApp app
+    )
+        external view override
+        returns(bool)
+    {
+        return SuperAppDefinitions.isAppJailed(_appManifests[app].configWord);
+    }
+
+    function allowCompositeApp(
+        ISuperApp targetApp
+    )
+        external override
+    {
+        ISuperApp sourceApp = ISuperApp(msg.sender);
+        if (!isApp(sourceApp)) revert HOST_SENDER_IS_NOT_SUPER_APP();
+        if (!isApp(targetApp)) revert HOST_RECEIVER_IS_NOT_SUPER_APP();
+        if (getAppCallbackLevel(sourceApp) <= getAppCallbackLevel(targetApp)) {
+            revert HOST_SOURCE_APP_NEEDS_HIGHER_APP_LEVEL();
+        } 
+        _compositeApps[sourceApp][targetApp] = true;
+    }
+
+    function isCompositeAppAllowed(
+        ISuperApp app,
+        ISuperApp targetApp
+    )
+        external view override
+        returns (bool)
+    {
+        return _compositeApps[app][targetApp];
+    }
+
+    /**************************************************************************
+     * Agreement Framework
+     *************************************************************************/
+
+    function callAppBeforeCallback(
+        ISuperApp app,
+        bytes calldata callData,
+        bool isTermination,
+        bytes calldata ctx
+    )
+        external override
+        onlyAgreement
+        assertValidCtx(ctx)
+        returns(bytes memory cbdata)
+    {
+        (bool success, bytes memory returnedData) = _callCallback(app, true, isTermination, callData, ctx);
+        if (success) {
+            if (CallUtils.isValidAbiEncodedBytes(returnedData)) {
+                cbdata = abi.decode(returnedData, (bytes));
+            } else {
+                if (!isTermination) {
+                    revert FluidspeedErrors.APP_RULE(SuperAppDefinitions.APP_RULE_CTX_IS_MALFORMATED);
+                } else {
+                    _jailApp(app, SuperAppDefinitions.APP_RULE_CTX_IS_MALFORMATED);
+                }
+            }
+        }
+    }
+
+    function callAppAfterCallback(
+        ISuperApp app,
+        bytes calldata callData,
+        bool isTermination,
+        bytes calldata ctx
+    )
+        external override
+        onlyAgreement
+        assertValidCtx(ctx)
+        returns(bytes memory newCtx)
+    {
+        (bool success, bytes memory returnedData) = _callCallback(app, false, isTermination, callData, ctx);
+        if (success) {
+            // the non static callback should not return empty ctx
+            if (CallUtils.isValidAbiEncodedBytes(returnedData)) {
+                newCtx = abi.decode(returnedData, (bytes));
+                if (!_isCtxValid(newCtx)) {
+                    if (!isTermination) {
+                        revert FluidspeedErrors.APP_RULE(SuperAppDefinitions.APP_RULE_CTX_IS_READONLY);
+                    } else {
+                        newCtx = ctx;
+                        _jailApp(app, SuperAppDefinitions.APP_RULE_CTX_IS_READONLY);
+                    }
+                }
+            } else {
+                if (!isTermination) {
+                    revert FluidspeedErrors.APP_RULE(SuperAppDefinitions.APP_RULE_CTX_IS_MALFORMATED);
+                } else {
+                    newCtx = ctx;
+                    _jailApp(app, SuperAppDefinitions.APP_RULE_CTX_IS_MALFORMATED);
+                }
+            }
+        } else {
+            newCtx = ctx;
+        }
+    }
+
+    function appCallbackPush(
+        bytes calldata ctx,
+        ISuperApp app,
+        uint256 appCreditGranted,
+        int256 appCreditUsed,
+        IFluidspeedToken appCreditToken
+    )
+        external override
+        onlyAgreement
+        assertValidCtx(ctx)
+        returns (bytes memory appCtx)
+    {
+        Context memory context = decodeCtx(ctx);
+        // NOTE: we use 1 as a magic number here as we want to do this check once we are in a callback
+        // we use 1 instead of MAX_APP_CALLBACK_LEVEL because 1 captures what we are trying to enforce
+        if (isApp(ISuperApp(context.msgSender)) && context.appCallbackLevel >= 1) {
+            if (!_compositeApps[ISuperApp(context.msgSender)][app]) {
+                revert FluidspeedErrors.APP_RULE(SuperAppDefinitions.APP_RULE_COMPOSITE_APP_IS_NOT_WHITELISTED);
+            }
+        }
+        context.appCallbackLevel++;
+        context.callType = ContextDefinitions.CALL_INFO_CALL_TYPE_APP_CALLBACK;
+        context.appCreditGranted = appCreditGranted;
+        context.appCreditUsed = appCreditUsed;
+        context.appAddress = address(app);
+        context.appCreditToken = appCreditToken;
+        appCtx = _updateContext(context);
+    }
+
+    function appCallbackPop(
+        bytes calldata ctx,
+        int256 appCreditUsedDelta
+    )
+        external override
+        onlyAgreement
+        returns (bytes memory newCtx)
+    {
+        Context memory context = decodeCtx(ctx);
+        context.appCreditUsed += appCreditUsedDelta;
+        newCtx = _updateContext(context);
+    }
+
+    function ctxUseCredit(
+        bytes calldata ctx,
+        int256 appCreditUsedMore
+    )
+        external override
+        onlyAgreement
+        assertValidCtx(ctx)
+        returns (bytes memory newCtx)
+    {
+        Context memory context = decodeCtx(ctx);
+        context.appCreditUsed += appCreditUsedMore;
+
+        newCtx = _updateContext(context);
+    }
+
+    function jailApp(
+        bytes calldata ctx,
+        ISuperApp app,
+        uint256 reason
+    )
+        external override
+        onlyAgreement
+        assertValidCtx(ctx)
+        returns (bytes memory newCtx)
+    {
+        _jailApp(app, reason);
+        return ctx;
+    }
+
+    /**************************************************************************
+    * Contextless Call Proxies
+    *************************************************************************/
+
+    function _callAgreement(
+        address msgSender,
+        ISuperAgreement agreementClass,
+        bytes memory callData,
+        bytes memory userData
+    )
+        internal
+        cleanCtx
+        isAgreement(agreementClass)
+        returns(bytes memory returnedData)
+    {
+        // beware of the endianness
+        bytes4 agreementSelector = CallUtils.parseSelector(callData);
+
+        //Build context data
+        bytes memory ctx = _updateContext(Context({
+            appCallbackLevel: 0,
+            callType: ContextDefinitions.CALL_INFO_CALL_TYPE_AGREEMENT,
+            timestamp: getNow(),
+            msgSender: msgSender,
+            agreementSelector: agreementSelector,
+            userData: userData,
+            appCreditGranted: 0,
+            appCreditWantedDeprecated: 0,
+            appCreditUsed: 0,
+            appAddress: address(0),
+            appCreditToken: IFluidspeedToken(address(0))
+        }));
+        bool success;
+        (success, returnedData) = _callExternalWithReplacedCtx(address(agreementClass), callData, ctx);
+        if (!success) {
+            CallUtils.revertFromReturnedData(returnedData);
+        }
+        // clear the stamp
+        _ctxStamp = 0;
+    }
+
+    function callAgreement(
+        ISuperAgreement agreementClass,
+        bytes memory callData,
+        bytes memory userData
+    )
+        external override
+        returns(bytes memory returnedData)
+    {
+        return _callAgreement(msg.sender, agreementClass, callData, userData);
+    }
+
+    function _callAppAction(
+        address msgSender,
+        ISuperApp app,
+        bytes memory callData
+    )
+        internal
+        cleanCtx
+        isAppActive(app)
+        isValidAppAction(callData)
+        returns(bytes memory returnedData)
+    {
+        // Build context data
+        bytes memory ctx = _updateContext(Context({
+            appCallbackLevel: 0,
+            callType: ContextDefinitions.CALL_INFO_CALL_TYPE_APP_ACTION,
+            timestamp: getNow(),
+            msgSender: msgSender,
+            agreementSelector: 0,
+            userData: "",
+            appCreditGranted: 0,
+            appCreditWantedDeprecated: 0,
+            appCreditUsed: 0,
+            appAddress: address(app),
+            appCreditToken: IFluidspeedToken(address(0))
+        }));
+        bool success;
+        (success, returnedData) = _callExternalWithReplacedCtx(address(app), callData, ctx);
+        if (success) {
+            ctx = abi.decode(returnedData, (bytes));
+            if (!_isCtxValid(ctx)) revert FluidspeedErrors.APP_RULE(SuperAppDefinitions.APP_RULE_CTX_IS_READONLY);
+        } else {
+            CallUtils.revertFromReturnedData(returnedData);
+        }
+        // clear the stamp
+        _ctxStamp = 0;
+    }
+
 }
